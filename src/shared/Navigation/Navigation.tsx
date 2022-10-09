01cb17504ac33d91ed1eb5418969950bfeb2d9f1
Vance Ingalls <vanceingalls@gmail.com>
2022-10-09 04:01:08 -0700

    wallet connect

--- src/shared/Navigation/Navigation.tsx
+++ src/shared/Navigation/Navigation.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import NavigationItem from "./NavigationItem";
 import { NAVIGATION_DEMO_2 } from "data/navigation";
+import MetamaskButton from "components/MetamaskButton";
+import ButtonPrimary from "shared/Button/ButtonPrimary";
+import { useProvider } from "hooks/useProvider";
 
 function Navigation() {
+  const { connected } = useProvider();
   return (
     <ul className="nc-Navigation hidden lg:flex lg:flex-wrap lg:items-center lg:space-x-1 relative">
+      {!connected ? (
+        <ButtonPrimary>
+          <MetamaskButton />
+        </ButtonPrimary>
+      ) : (
+        <MetamaskButton />
+      )}
+      <div className="mr-15"></div>
       {NAVIGATION_DEMO_2.map((item) => (
         <NavigationItem key={item.id} menuItem={item} />
       ))}
