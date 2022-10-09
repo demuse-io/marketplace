a552c93dcd2e5ace962e8f64e43414928fe2181a
Vance Ingalls <vanceingalls@gmail.com>
2022-10-09 05:55:04 -0700

    light mode logo

--- src/shared/Logo/Logo.tsx
+++ src/shared/Logo/Logo.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logoImg from "images/logo_white.png";
+import logoImg from "images/logo_black_pink.png";
+import logoLightImg from "images/logo_white.png";
 
 export interface LogoProps {
   img?: string;
@@ -8,12 +9,13 @@ export interface LogoProps {
   className?: string;
 }
 
-const Logo: React.FC<LogoProps> = ({ img = logoImg, className = "" }) => {
+const Logo: React.FC<LogoProps> = ({ img = logoImg, imgLight = logoLightImg, className = "" }) => {
   return (
     <Link to="/" className={`ttnc-logo inline-block text-primary-6000 ${className}`}>
       {/* THIS USE FOR MY CLIENT */}
       {/* PLEASE UN COMMENT BELLOW CODE AND USE IT */}
-      {img ? <img className={`block max-h-12`} src={img} alt="Logo" /> : "Logo Here"}
+      {img ? <img className={`block max-h-12 ${imgLight ? "dark:hidden" : ""}`} src={img} alt="Logo" /> : "Logo Here"}
+      {imgLight && <img className="hidden max-h-12 dark:block" src={imgLight} alt="Logo-Light" />}
     </Link>
   );
 };
