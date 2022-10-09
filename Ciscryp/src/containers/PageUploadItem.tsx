dd65e115289e8a620e31995a12520088ea1039ac
migrenaa <mimonova13@gmail.com>
2022-10-09 02:10:55 -0500

    initial commit

---
+++ Ciscryp/src/containers/PageUploadItem.tsx
@@ -0,0 +1,178 @@
+import Label from "components/Label/Label";
+import React, { FC, useState } from "react";
+import ButtonPrimary from "shared/Button/ButtonPrimary";
+import Input from "shared/Input/Input";
+import Textarea from "shared/Textarea/Textarea";
+import { Helmet } from "react-helmet";
+import FormItem from "components/FormItem";
+import { nftsImgs } from "contains/fakeData";
+import { presaleAbi } from "abis/presale.abi";
+export interface PageUploadItemProps {
+  className?: string;
+}
+
+const plans = [
+  {
+    name: "Crypto Legend - Professor",
+    featuredImage: nftsImgs[0],
+  },
+  {
+    name: "Crypto Legend - Professor",
+    featuredImage: nftsImgs[1],
+  },
+  {
+    name: "Crypto Legend - Professor",
+    featuredImage: nftsImgs[2],
+  },
+  {
+    name: "Crypto Legend - Professor",
+    featuredImage: nftsImgs[3],
+  },
+  {
+    name: "Crypto Legend - Professor",
+    featuredImage: nftsImgs[4],
+  },
+  {
+    name: "Crypto Legend - Professor",
+    featuredImage: nftsImgs[5],
+  },
+];
+
+const PageUploadItem: FC<PageUploadItemProps> = ({ className = "" }) => {
+  const [selected, setSelected] = useState(plans[1]);
+
+  async function mintPresale() {
+    // presaleContract()
+  }
+  return (
+    <div
+      className={`nc-PageUploadItem ${className}`}
+      data-nc-id="PageUploadItem"
+    >
+      <Helmet>
+        <title>Create Item || NFT React Template</title>
+      </Helmet>
+      <div className="container">
+        <div className="my-12 sm:lg:my-16 lg:my-24 max-w-4xl mx-auto space-y-8 sm:space-y-10">
+          {/* HEADING */}
+          <div className="max-w-2xl">
+            <h2 className="text-3xl sm:text-4xl font-semibold">
+              Presale your NFT
+            </h2>
+            <span className="block mt-3 text-neutral-500 dark:text-neutral-400">
+              You can set preferred display name, create your profile URL and
+              manage other personal settings.
+            </span>
+          </div>
+          <div className="w-full border-b-2 border-neutral-100 dark:border-neutral-700"></div>
+          <div className="mt-10 md:mt-0 space-y-5 sm:space-y-6 md:sm:space-y-8">
+            <div>
+              <h3 className="text-lg sm:text-2xl font-semibold">
+                Image, Video, Audio, or 3D Model
+              </h3>
+              <span className="text-neutral-500 dark:text-neutral-400 text-sm">
+                File types supported: JPG, PNG, GIF, SVG, MP4, WEBM, MP3, WAV,
+                OGG, GLB, GLTF. Max size: 100 MB
+              </span>
+              <div className="mt-5 ">
+                <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-neutral-300 dark:border-neutral-6000 border-dashed rounded-xl">
+                  <div className="space-y-1 text-center">
+                    <svg
+                      className="mx-auto h-12 w-12 text-neutral-400"
+                      stroke="currentColor"
+                      fill="none"
+                      viewBox="0 0 48 48"
+                      aria-hidden="true"
+                    >
+                      <path
+                        d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      ></path>
+                    </svg>
+                    <div className="flex text-sm text-neutral-6000 dark:text-neutral-300">
+                      <label
+                        htmlFor="file-upload"
+                        className="relative cursor-pointer  rounded-md font-medium text-primary-6000 hover:text-primary-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-primary-500"
+                      >
+                        <span>Upload a file</span>
+                        <input
+                          id="file-upload"
+                          name="file-upload"
+                          type="file"
+                          className="sr-only"
+                        />
+                      </label>
+                      <p className="pl-1">or drag and drop</p>
+                    </div>
+                    <p className="text-xs text-neutral-500 dark:text-neutral-400">
+                      PNG, JPG, GIF up to 10MB
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* ---- */}
+            <FormItem label="Item Name">
+              <Input defaultValue="NFT name" />
+            </FormItem>
+
+            {/* ---- */}
+            <FormItem
+              label="Description"
+              desc={
+                <div>
+                  The description will be included on the item's detail page
+                  underneath its image.{" "}
+                  <span className="text-green-500">Markdown</span> syntax is
+                  supported.
+                </div>
+              }
+            >
+              <Textarea rows={6} className="mt-1.5" placeholder="..." />
+            </FormItem>
+
+            <div className="w-full border-b-2 border-neutral-100 dark:border-neutral-700"></div>
+
+            {/* ---- */}
+            <div className="grid grid-cols-1 sm:grid-cols-3 gap-5 sm:gap-2.5">
+              {/* ---- */}
+              <FormItem label="Shares to sell">
+                <Input placeholder="20%" />
+              </FormItem>
+              {/* ---- */}
+              <FormItem label="Total amount to crowdfund">
+                <Input placeholder="$1000" />
+              </FormItem>
+            </div>
+
+            {/* ---- */}
+            <div className="pt-2 flex flex-col sm:flex-row space-y-3 sm:space-y-0 space-x-0 sm:space-x-3 ">
+              <ButtonPrimary className="flex-1" onClick={() => mintPresale()}>Upload item</ButtonPrimary>
+              {/* <ButtonSecondary className="flex-1">Preview item</ButtonSecondary> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+function CheckIcon(props: any) {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" {...props}>
+      <circle cx={12} cy={12} r={12} fill="#fff" opacity="0.2" />
+      <path
+        d="M7 13l3 3 7-7"
+        stroke="#fff"
+        strokeWidth={1.5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default PageUploadItem;
---
+++ Ciscryp/src/contracts/presale.contract.tsx
@@ -0,0 +1,13 @@
+import { ethers } from "ethers";
+import { presaleAbi } from "../abis/presale.abi";
+const presaleAddress = "0x9F245626e59A091f8E47883C530E09E11e87f457";
+import { NFTStorage } from "nft.storage";
+const nftStorage = new NFTStorage({ token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEY5MmY3MTg0M0Y4MzkzMjU5MjlmOGNGMzIzNThkMkY2RmNGNUU3RjkiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY2NTI5MTM5NDI3NSwibmFtZSI6ImRlbXVzZSJ9.abZfTRzXNEU1onl-fqHfdsyv1RfPpGsePrDsS3UM3dU' });
+
+export const presaleContract = (signer: any) =>
+  new ethers.Contract(presaleAddress, presaleAbi, signer);
+
+export const mint = async () => {
+
+
+};
